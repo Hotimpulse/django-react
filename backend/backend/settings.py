f1d417a0ad9ee889c1435dc0c7f82dda882a9dc3
Hotimpulse <hotimpulse@hotmail.com>
2025-11-26 00:29:08 +0300

    refactor: changing out the db to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,12 +96,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_react',
-        'USER': 'postgres',
-        'PASSWORD': 'Hello123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PWD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
